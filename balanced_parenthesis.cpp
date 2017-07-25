b9df68c4bdf5d03efbdab9f64236348e2c9866e0
pratiproychowdhury <pratip.roy.chowdhury@gmail.com>
2017-07-25 19:03:57 -0300

    Included balanced parenthesis

--- balanced_parenthesis.cpp
+++ balanced_parenthesis.cpp
@@ -1,4 +1,59 @@
-int main() {
+bool is_balanced(string expression) {
 
-  return 0;
+stack<char> brackets;
+
+   for (int i=0; i<expression.size(); i++) {
+
+        if ((expression[i] == '(') || (expression[i] == '[') || (expression[i] == '{')) {
+                        brackets.push(expression[i]);
+                        //cout << "Stacking " << expression[i] << "\n";
+        }
+
+        if (expression[i] == ')') {
+                        if (brackets.empty())
+                                return false;
+                        if (brackets.top() == '(') {
+                                brackets.pop();
+                                //cout << "Popping (\n";
+                        }
+                        else {
+                                //cout << "Looking for a ( in stack but not found. Found a " << brackets.top() << "\n";
+                                return false;
+                        }
+        }
+
+        if (expression[i] == ']') {
+                        if (brackets.empty())
+                                return false;
+                        if (brackets.top() == '[') {
+                                brackets.pop();
+                                //cout << "Popping [\n";
+                        }
+                        else {
+                                //cout << "Looking for a [ in stack but not found. Found a " << brackets.top() << "\n";
+                                return false;
+                        }
+        }
+
+        if (expression[i] == '}') {
+                        if (brackets.empty())
+                                return false;
+                        if (brackets.top() == '{') {
+                                brackets.pop();
+                                //cout << "Popping {\n";
+                        }
+                        else {
+                                //cout << "Looking for a { in stack but not found. Found a " << brackets.top() << "\n";
+                                return false;
+                        }
+        }
+
 }
+
+if (brackets.empty())
+        return true;
+
+return false;
+}
+
+
