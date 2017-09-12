94774548d35fef981e8e244f5933480db0ba9540
pratiproychowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2017-09-12 01:38:36 -0300

    Update and rename ransom_note.cpp to hackerrank/ransom_note.cpp

--- hackerrank/ransom_note.cpp
+++ hackerrank/ransom_note.cpp
@@ -5,15 +5,15 @@ bool ransom_note(vector<string> magazine, vector<string> ransom) {
 	multiset<string>::iterator thisword;
     
 	for (word=magazine.begin(); word!=magazine.end(); ++word)
-    	dictionary.insert(*word);
+    		dictionary.insert(*word);
 	cout << dictionary.size() << "\n";
 	for (word=ransom.begin(); word!=ransom.end(); ++word) {
-    	cout << "looking for " << *word << "\n";
-    	thisword = dictionary.find(*word);
-    	if (thisword == dictionary.end())
-        	return false;
-    	dictionary.erase(thisword);
-    	cout << dictionary.size() << "\n";
+    		cout << "looking for " << *word << "\n";
+    		thisword = dictionary.find(*word);
+    		if (thisword == dictionary.end())
+        		return false;
+    		dictionary.erase(thisword);
+    		cout << dictionary.size() << "\n";
 	}
 	return true;
 }
