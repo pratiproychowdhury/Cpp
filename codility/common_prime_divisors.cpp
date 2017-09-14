c6a0f9874c86e23744d3aaba45ddb8cac585324f
pratiproychowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2017-09-13 23:40:00 -0300

    Create common_prime_divisors.cpp

---
+++ codility/common_prime_divisors.cpp
@@ -0,0 +1,69 @@
+/*
+A prime is a positive integer X that has exactly two distinct divisors: 1 and X. The first few prime integers are 2, 3, 5, 7, 11 and 13.
+
+A prime D is called a prime divisor of a positive integer P if there exists a positive integer K such that D * K = P. For example, 2 and 5 are prime divisors of 20.
+
+You are given two positive integers N and M. The goal is to check whether the sets of prime divisors of integers N and M are exactly the same.
+
+For example, given:
+
+N = 15 and M = 75, the prime divisors are the same: {3, 5};
+N = 10 and M = 30, the prime divisors aren't the same: {2, 5} is not equal to {2, 3, 5};
+N = 9 and M = 5, the prime divisors aren't the same: {3} is not equal to {5}.
+Write a function:
+
+int solution(vector<int> &A, vector<int> &B);
+
+that, given two non-empty zero-indexed arrays A and B of Z integers, returns the number of positions K for which the prime divisors of A[K] and B[K] are exactly the same.
+
+For example, given:
+
+    A[0] = 15   B[0] = 75
+    A[1] = 10   B[1] = 30
+    A[2] = 3    B[2] = 5
+the function should return 1, because only one pair (15, 75) has the same set of prime divisors.
+
+Assume that:
+
+Z is an integer within the range [1..6,000];
+each element of arrays A, B is an integer within the range [1..2,147,483,647].
+Complexity:
+
+expected worst-case time complexity is O(Z*log(max(A)+max(B))2);
+expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
+*/
+
+#include <set>
+#include <algorithm>
+
+void factorize(set<int> &factors, int x) {
+    
+    for (int i=2; i<=(x/2); i++) {
+        if ((x%i) == 0) {
+            factors.emplace(i);
+            x /= i;
+            i--;
+        }
+        if (x==1)
+            break;
+    }
+    if (x>1)
+        factors.emplace(x);
+}
+
+
+int solution(vector<int> &A, vector<int> &B) {
+
+    int same_divisors = 0;
+    
+    for (unsigned int i=0; i<A.size(); i++) {
+        set<int> factors_a, factors_b;
+        factorize(factors_a, A[i]);
+        factorize(factors_b, B[i]);
+
+        if (factors_a == factors_b)
+            same_divisors++;
+    }
+    
+    return same_divisors;
+}
