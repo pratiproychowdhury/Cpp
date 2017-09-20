cf29b368d839edd4ae7d08a8cd56d56cac3b3b1c
pratiproychowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2017-09-20 02:01:19 -0300

    Update check_primality.cpp

--- hackerrank/check_primality.cpp
+++ hackerrank/check_primality.cpp
@@ -1,13 +1,6 @@
-#include <map>
-#include <set>
-#include <list>
 #include <cmath>
 #include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
 #include <string>
-#include <bitset>
 #include <cstdio>
 #include <limits>
 #include <vector>
@@ -18,11 +11,8 @@
 #include <numeric>
 #include <sstream>
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
 
 using namespace std;
-
 
 int main(){
     int p;
