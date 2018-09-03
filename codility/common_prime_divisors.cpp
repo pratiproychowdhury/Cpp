9087ba2a382071d3b3c743d77f16e02e4f9b50db
pratiproychowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2018-09-03 18:58:43 -0300

    Update common_prime_divisors.cpp

--- codility/common_prime_divisors.cpp
+++ codility/common_prime_divisors.cpp
@@ -1,9 +1,12 @@
 /*
-A prime is a positive integer X that has exactly two distinct divisors: 1 and X. The first few prime integers are 2, 3, 5, 7, 11 and 13.
+A prime is a positive integer X that has exactly two distinct divisors: 1 and X. The first few prime integers are 2, 3, 5, 7, 
+11 and 13.
 
-A prime D is called a prime divisor of a positive integer P if there exists a positive integer K such that D * K = P. For example, 2 and 5 are prime divisors of 20.
+A prime D is called a prime divisor of a positive integer P if there exists a positive integer K such that D * K = P. For 
+example, 2 and 5 are prime divisors of 20.
 
-You are given two positive integers N and M. The goal is to check whether the sets of prime divisors of integers N and M are exactly the same.
+You are given two positive integers N and M. The goal is to check whether the sets of prime divisors of integers N and M are 
+exactly the same.
 
 For example, given:
 
@@ -14,7 +17,8 @@ Write a function:
 
 int solution(vector<int> &A, vector<int> &B);
 
-that, given two non-empty zero-indexed arrays A and B of Z integers, returns the number of positions K for which the prime divisors of A[K] and B[K] are exactly the same.
+that, given two non-empty zero-indexed arrays A and B of Z integers, returns the number of positions K for which the prime 
+divisors of A[K] and B[K] are exactly the same.
 
 For example, given:
 
