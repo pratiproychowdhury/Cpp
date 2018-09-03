895940d5a5013bc8da81f4f28215bdb043e7f934
pratiproychowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2018-09-03 19:36:34 -0300

    Update tie_ropes.cpp

--- codility/tie_ropes.cpp
+++ codility/tie_ropes.cpp
@@ -1,9 +1,12 @@
 /*
-There are N ropes numbered from 0 to N − 1, whose lengths are given in a zero-indexed array A, lying on the floor in a line. For each I (0 ≤ I < N), the length of rope I on the line is A[I].
+There are N ropes numbered from 0 to N − 1, whose lengths are given in a zero-indexed array A, lying on the floor in a line. 
+For each I (0 ≤ I < N), the length of rope I on the line is A[I].
 
-We say that two ropes I and I + 1 are adjacent. Two adjacent ropes can be tied together with a knot, and the length of the tied rope is the sum of lengths of both ropes. The resulting new rope can then be tied again.
+We say that two ropes I and I + 1 are adjacent. Two adjacent ropes can be tied together with a knot, and the length of the 
+tied rope is the sum of lengths of both ropes. The resulting new rope can then be tied again.
 
-For a given integer K, the goal is to tie the ropes in such a way that the number of ropes whose length is greater than or equal to K is maximal.
+For a given integer K, the goal is to tie the ropes in such a way that the number of ropes whose length is greater than or 
+equal to K is maximal.
 
 For example, consider K = 4 and array A such that:
 
@@ -22,13 +25,15 @@ We can tie:
 
 rope 1 with rope 2 to produce a rope of length A[1] + A[2] = 5;
 rope 4 with rope 5 with rope 6 to produce a rope of length A[4] + A[5] + A[6] = 5.
-After that, there will be three ropes whose lengths are greater than or equal to K = 4. It is not possible to produce four such ropes.
+After that, there will be three ropes whose lengths are greater than or equal to K = 4. It is not possible to produce four 
+such ropes.
 
 Write a function:
 
 int solution(int K, vector<int> &A);
 
-that, given an integer K and a non-empty zero-indexed array A of N integers, returns the maximum number of ropes of length greater than or equal to K that can be created.
+that, given an integer K and a non-empty zero-indexed array A of N integers, returns the maximum number of ropes of length 
+greater than or equal to K that can be created.
 
 For example, given K = 4 and array A such that:
 
