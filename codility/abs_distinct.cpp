dae491ae63be5ff7e05d5429ba1557845fe20aab
pratiproychowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2018-09-03 18:57:59 -0300

    Update abs_distinct.cpp

--- codility/abs_distinct.cpp
+++ codility/abs_distinct.cpp
@@ -1,5 +1,6 @@
 /*
-A non-empty zero-indexed array A consisting of N numbers is given. The array is sorted in non-decreasing order. The absolute distinct count of this array is the number of distinct absolute values among the elements of the array.
+A non-empty zero-indexed array A consisting of N numbers is given. The array is sorted in non-decreasing order. The absolute 
+distinct count of this array is the number of distinct absolute values among the elements of the array.
 
 For example, consider array A such that:
 
@@ -9,7 +10,8 @@ For example, consider array A such that:
   A[3] =  0
   A[4] =  3
   A[5] =  6
-The absolute distinct count of this array is 5, because there are 5 distinct absolute values among the elements of this array, namely 0, 1, 3, 5 and 6.
+The absolute distinct count of this array is 5, because there are 5 distinct absolute values among the elements of this array, 
+namely 0, 1, 3, 5 and 6.
 
 Write a function:
 
