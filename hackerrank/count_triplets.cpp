21ca381f8c2f73d157da070d054d50bfc4e409e1
pratiproychowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2021-03-10 15:35:14 +0100

    Create count_triplets.cpp

---
+++ hackerrank/count_triplets.cpp
@@ -0,0 +1,159 @@
+/*
+You are given an array and you need to find number of tripets of indices  such that the elements at those indices are in geometric progression for a given common ratio  and .
+
+For example, . If , we have  and  at indices  and .
+
+Function Description
+
+Complete the countTriplets function in the editor below. It should return the number of triplets forming a geometric progression for a given  as an integer.
+
+countTriplets has the following parameter(s):
+
+arr: an array of integers
+r: an integer, the common ratio
+Input Format
+
+The first line contains two space-separated integers  and , the size of  and the common ratio.
+The next line contains  space-seperated integers .
+
+Constraints
+
+Output Format
+
+Return the count of triplets that form a geometric progression.
+
+Sample Input 0
+
+4 2
+1 2 2 4
+Sample Output 0
+
+2
+Explanation 0
+
+There are  triplets in satisfying our criteria, whose indices are  and 
+
+Sample Input 1
+
+6 3
+1 3 9 9 27 81
+Sample Output 1
+
+6
+Explanation 1
+
+The triplets satisfying are index , , , ,  and .
+
+Sample Input 2
+
+5 5
+1 5 5 25 125
+Sample Output 2
+
+4
+Explanation 2
+
+The triplets satisfying are index , , , .
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+// Complete the countTriplets function below.
+long countTriplets(vector<long> arr, long r) {
+    unordered_multimap<long, long> data;
+    long triplets = 0;
+    
+    for (int i=0; i<arr.size(); i++)
+        data.emplace(arr[i], i);
+    
+    for (int i=0; i<arr.size()-2; i++) {
+        auto square = data.equal_range(arr[i]*r);
+        auto cube = data.equal_range(arr[i]*r*r);
+        for (auto m=square.first; m!=square.second; m++)
+            for (auto n=cube.first; n!=cube.second; n++) {
+              if ( (m->second > i) && (n->second > m->second) )
+                triplets++;
+            }     
+    }    
+    return triplets;  
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nr_temp;
+    getline(cin, nr_temp);
+
+    vector<string> nr = split(rtrim(nr_temp));
+
+    int n = stoi(nr[0]);
+
+    long r = stol(nr[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<long> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        long arr_item = stol(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    long ans = countTriplets(arr, r);
+
+    fout << ans << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
