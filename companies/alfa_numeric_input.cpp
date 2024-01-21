8352e2c1b5b207911d256ba8e3658dff1841477f
pratiproychowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2024-01-21 05:28:44 +0100

    Create alfa_numeric_input.cpp

---
+++ companies/alfa_numeric_input.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+class TextInput
+{
+protected:
+  std::string text;
+  
+public:
+    virtual void add(char c) { text += c; }
+
+    std::string getValue() { return text; }
+};
+
+class NumericInput : public TextInput { 
+
+    void add(char c) { 
+      if (('0' <= c) && (c <= '9'))
+        text += c;
+    }
+
+};
+
+#ifndef RunTests
+int main()
+{
+    TextInput* input = new NumericInput();
+    input->add('1');
+    input->add('a');
+    input->add('0');
+    std::cout << input->getValue();
+}
+#endif
