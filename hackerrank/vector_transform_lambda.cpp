d334670db86b4d738271bd4d742cf2267b5a9cbd
Pratip Roy Chowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2025-03-31 01:27:57 +0200

    Create vector_transform_lambda.cpp

---
+++ hackerrank/vector_transform_lambda.cpp
@@ -0,0 +1,162 @@
+/*
+Starting with a 1-indexed array of zeros and a list of operations, for each operation add a value to each array element between two given indices, inclusive. Once all operations have been performed, return the maximum value in the array.
+
+Example
+
+
+Queries are interpreted as follows:
+
+    a b k
+    1 5 3
+    4 8 7
+    6 9 1
+Add the values of  between the indices  and  inclusive:
+
+image
+
+The largest value is  after all operations are performed.
+
+Function Description
+
+Complete the function  with the following parameters:
+
+: the number of elements in the array
+: a two dimensional array of queries where each  contains three integers, , , and .
+Returns
+
+: the maximum value in the resultant array
+Input Format
+
+The first line contains two space-separated integers  and , the size of the array and the number of queries.
+Each of the next  lines contains three space-separated integers ,  and , the left index, right index and number to add.
+
+Constraints
+
+Sample Input
+
+STDIN       Function
+-----       --------
+5 3         arr[] size n = 5, queries[] size q = 3
+1 2 100     queries = [[1, 2, 100], [2, 5, 100], [3, 4, 100]]
+2 5 100
+3 4 100
+Sample Output
+
+200
+Explanation
+
+After the first update the list is 100 100 0 0 0.
+After the second update list is 100 200 100 100 100.
+After the third update list is 100 200 200 200 100.
+
+The maximum value is .
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'arrayManipulation' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+long arrayManipulation(int n, vector<vector<int>> queries) {
+
+    vector<long> data(n, 0);
+    for (vector<int> query : queries) {
+        //for (int i=query[0]-1; i<query[1]; i++)
+        //    data[i] += query[2];
+        auto a = data.begin()+(query[0]-1);
+        auto b = data.begin()+query[1];
+        transform (a, b, a, [&query](long i) -> long {return query[2] + i;} );
+    }
+
+    return *max_element(data.begin(), data.end());
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int m = stoi(first_multiple_input[1]);
+
+    vector<vector<int>> queries(m);
+
+    for (int i = 0; i < m; i++) {
+        queries[i].resize(3);
+
+        string queries_row_temp_temp;
+        getline(cin, queries_row_temp_temp);
+
+        vector<string> queries_row_temp = split(rtrim(queries_row_temp_temp));
+
+        for (int j = 0; j < 3; j++) {
+            int queries_row_item = stoi(queries_row_temp[j]);
+
+            queries[i][j] = queries_row_item;
+        }
+    }
+
+    long result = arrayManipulation(n, queries);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
