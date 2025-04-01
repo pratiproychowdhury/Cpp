8fe8d22e0e4f19132c55962764ebbafd4e801411
Pratip Roy Chowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2025-04-02 01:53:02 +0200

    Create game_of_two_stacks_max_int.cpp

---
+++ hackerrank/game_of_two_stacks_max_int.cpp
@@ -0,0 +1,196 @@
+/*
+Alexa has two stacks of non-negative integers, stack  and stack  where index  denotes the top of the stack. Alexa challenges Nick to play the following game:
+
+In each move, Nick can remove one integer from the top of either stack  or stack .
+Nick keeps a running sum of the integers he removes from the two stacks.
+Nick is disqualified from the game if, at any point, his running sum becomes greater than some integer  given at the beginning of the game.
+Nick's final score is the total number of integers he has removed from the two stacks.
+Given , , and  for  games, find the maximum possible score Nick can achieve.
+
+Example
+
+
+The maximum number of values Nick can remove is . There are two sets of choices with this result.
+
+Remove  from  with a sum of .
+Remove  from  and  from  with a sum of .
+Function Description
+Complete the twoStacks function in the editor below.
+
+twoStacks has the following parameters: - int maxSum: the maximum allowed sum
+- int a[n]: the first stack
+- int b[m]: the second stack
+
+Returns
+- int: the maximum number of selections Nick can make
+
+Input Format
+
+The first line contains an integer,  (the number of games). The  subsequent lines describe each game in the following format:
+
+The first line contains three space-separated integers describing the respective values of  (the number of integers in stack ),  (the number of integers in stack ), and  (the number that the sum of the integers removed from the two stacks cannot exceed).
+The second line contains  space-separated integers, the respective values of .
+The third line contains  space-separated integers, the respective values of .
+Constraints
+
+Subtasks
+
+ for  of the maximum score.
+Sample Input 0
+
+1
+5 4 10
+4 2 4 6 1
+2 1 8 5
+Sample Output 0
+
+4
+Explanation 0
+
+The two stacks initially look like this:
+
+image
+
+The image below depicts the integers Nick should choose to remove from the stacks. We print  as our answer, because that is the maximum number of integers that can be removed from the two stacks without the sum exceeding .
+
+image
+
+(There can be multiple ways to remove the integers from the stack, the image shows just one of them.)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'twoStacks' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER maxSum
+ *  2. INTEGER_ARRAY a
+ *  3. INTEGER_ARRAY b
+ */
+
+int twoStacks(int maxSum, vector<int> a, vector<int> b) {
+
+    int sum = 0;
+    int terms = 0;
+    a.emplace_back(numeric_limits<int>::max());
+    b.emplace_back(numeric_limits<int>::max());
+    auto aptr = a.begin();
+    auto bptr = b.begin();
+    
+    while (sum <= maxSum) {
+        if (*aptr < *bptr) {
+            sum += *aptr;
+            aptr++;            
+        }
+        else {
+            sum += *bptr;
+            bptr++;            
+        }
+        terms++;
+    }
+    return terms-1;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string g_temp;
+    getline(cin, g_temp);
+
+    int g = stoi(ltrim(rtrim(g_temp)));
+
+    for (int g_itr = 0; g_itr < g; g_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int n = stoi(first_multiple_input[0]);
+
+        int m = stoi(first_multiple_input[1]);
+
+        int maxSum = stoi(first_multiple_input[2]);
+
+        string a_temp_temp;
+        getline(cin, a_temp_temp);
+
+        vector<string> a_temp = split(rtrim(a_temp_temp));
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++) {
+            int a_item = stoi(a_temp[i]);
+
+            a[i] = a_item;
+        }
+
+        string b_temp_temp;
+        getline(cin, b_temp_temp);
+
+        vector<string> b_temp = split(rtrim(b_temp_temp));
+
+        vector<int> b(m);
+
+        for (int i = 0; i < m; i++) {
+            int b_item = stoi(b_temp[i]);
+
+            b[i] = b_item;
+        }
+
+        int result = twoStacks(maxSum, a, b);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
