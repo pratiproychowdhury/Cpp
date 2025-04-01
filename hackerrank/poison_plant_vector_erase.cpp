6dae01084a3b1afafbf338adebccc14441a45ef6
Pratip Roy Chowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2025-04-02 01:29:37 +0200

    Create poison_plant_vector_erase.cpp

---
+++ hackerrank/poison_plant_vector_erase.cpp
@@ -0,0 +1,164 @@
+/*
+There are a number of plants in a garden. Each of the plants has been treated with some amount of pesticide. After each day, if any plant has more pesticide than the plant on its left, being weaker than the left one, it dies.
+
+You are given the initial values of the pesticide in each of the plants. Determine the number of days after which no plant dies, i.e. the time after which there is no plant with more pesticide content than the plant to its left.
+
+Example
+
+ // pesticide levels
+
+Use a -indexed array. On day , plants  and  die leaving . On day , plant  in  dies leaving . There is no plant with a higher concentration of pesticide than the one to its left, so plants stop dying after day .
+
+Function Description
+Complete the function poisonousPlants in the editor below.
+
+poisonousPlants has the following parameter(s):
+
+int p[n]: the pesticide levels in each plant
+Returns
+- int: the number of days until plants no longer die from pesticide
+
+Input Format
+
+The first line contains an integer , the size of the array .
+The next line contains  space-separated integers .
+
+Constraints
+
+
+
+Sample Input
+
+7
+6 5 8 4 7 10 9
+Sample Output
+
+2
+Explanation
+
+Initially all plants are alive.
+
+Plants = {(6,1), (5,2), (8,3), (4,4), (7,5), (10,6), (9,7)}
+
+Plants[k] = (i,j) => jth plant has pesticide amount = i.
+
+After the 1st day, 4 plants remain as plants 3, 5, and 6 die.
+
+Plants = {(6,1), (5,2), (4,4), (9,7)}
+
+After the 2nd day, 3 plants survive as plant 7 dies.
+
+Plants = {(6,1), (5,2), (4,4)}
+
+Plants stop dying after the 2nd day.
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'poisonousPlants' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY p as parameter.
+ */
+
+int poisonousPlants(vector<int> p) {
+    int day = 0;
+    bool dead;
+    
+    do {
+        day++;
+        dead = false;
+        auto ptr = p.begin()+1;
+        int previous = *(p.begin());
+        while (ptr != p.end()) {
+            if (*ptr > previous) {
+                previous = *ptr;
+                ptr = p.erase(ptr);
+                dead = true;
+            }
+            else {
+                previous = *ptr;
+                ptr++;
+            }
+        }
+    } while (dead);
+    
+    return day-1;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string p_temp_temp;
+    getline(cin, p_temp_temp);
+
+    vector<string> p_temp = split(rtrim(p_temp_temp));
+
+    vector<int> p(n);
+
+    for (int i = 0; i < n; i++) {
+        int p_item = stoi(p_temp[i]);
+
+        p[i] = p_item;
+    }
+
+    int result = poisonousPlants(p);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
