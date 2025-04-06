247d322e3016f5dd97ec63b1d54b49063cfbe453
Pratip Roy Chowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2025-04-06 23:32:42 +0200

    Create egy_bogo.cpp

---
+++ companies/egy_bogo.cpp
@@ -0,0 +1,39 @@
+# BogoSort
+def sort(xs):
+ 	while not sorted(xs):
+   		random_shuffle(xs)
+
+
+
+Person* getPerson() {
+  	Person* p = new Person();
+  	return &p;
+}
+
+// options:
+//   - new
+//   - static
+//   - member
+
+unique_ptr<Person> 
+
+
+
+
+vector <products> prod;
+// prod.version
+
+operator< (products& a, products& b);
+
+bool sorter(products& a, products& b) {
+  if (a.version.major > b.version.major)
+  	return false;
+  	else if (b.version.major < a.version.major)
+  		return true;
+  	else 
+  		return a.version.minor < a.version.major;
+}
+
+sort(prod.begin(), prod.end(), sorter());
+
+
