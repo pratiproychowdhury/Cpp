fca41dce7a475b7638113cd618717237d45fae9b
Pratip Roy Chowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2025-04-17 00:55:14 +0200

    Update egy_bogo.cpp

--- companies/egy_bogo.cpp
+++ companies/egy_bogo.cpp
@@ -15,7 +15,7 @@ Person* getPerson() {
 //   - static
 //   - member
 
-unique_ptr<Person> 
+unique_ptr<Person> p = make_unique<Person>();
 
 
 
@@ -27,13 +27,13 @@ operator< (products& a, products& b);
 
 bool sorter(products& a, products& b) {
   if (a.version.major > b.version.major)
-  	return false;
-  	else if (b.version.major < a.version.major)
+  	 return false;
+  else if (b.version.major < a.version.major)
   		return true;
-  	else 
+  else 
   		return a.version.minor < a.version.major;
 }
 
-sort(prod.begin(), prod.end(), sorter());
+sort(prod.begin(), prod.end(), sorter);
 
 
