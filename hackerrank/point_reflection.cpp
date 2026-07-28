405182f5824101a0cfe2f9a9b45ac47d31ab2d97
Pratip Roy Chowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2026-07-28 21:28:17 +0100

    Add findPoint function for point reflection
    Implement point reflection calculation for given points.

---
+++ hackerrank/point_reflection.cpp
@@ -0,0 +1,142 @@
+/*
+Consider two points,  and . We consider the inversion or point reflection, , of point  across point  to be a  rotation of point  around .
+
+Given  sets of points  and , find  for each pair of points and print two space-separated integers denoting the respective values of  and  on a new line.
+
+Function Description
+
+Complete the findPoint function in the editor below.
+
+findPoint has the following parameters:
+
+int px, py, qx, qy: x and y coordinates for points  and 
+Returns
+
+int[2]: x and y coordinates of the reflected point 
+Input Format
+
+The first line contains an integer, , denoting the number of sets of points.
+Each of the  subsequent lines contains four space-separated integers that describe the respective values of , , , and  defining points  and .
+
+Constraints
+
+Sample Input
+
+2
+0 0 1 1
+1 1 2 2
+Sample Output
+
+2 2
+3 3
+Explanation
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'findPoint' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER px
+ *  2. INTEGER py
+ *  3. INTEGER qx
+ *  4. INTEGER qy
+ */
+
+vector<int> findPoint(int px, int py, int qx, int qy) {
+    vector<int> answer;
+    
+    answer.push_back(qx+(qx-px));
+    answer.push_back(qy+(qy-py));
+    
+    return answer;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    for (int n_itr = 0; n_itr < n; n_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int px = stoi(first_multiple_input[0]);
+
+        int py = stoi(first_multiple_input[1]);
+
+        int qx = stoi(first_multiple_input[2]);
+
+        int qy = stoi(first_multiple_input[3]);
+
+        vector<int> result = findPoint(px, py, qx, qy);
+
+        for (size_t i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
