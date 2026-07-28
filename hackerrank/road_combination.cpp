888857216d9ed3281226715b873f3c5a6ca49fbc
Pratip Roy Chowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2026-07-28 21:37:24 +0100

    Add connectingTowns function for route calculation
    Implement the connectingTowns function to calculate the total number of routes from the first to the last town, using the provided routes array. The result is returned modulo 1234567.

---
+++ hackerrank/road_combination.cpp
@@ -0,0 +1,154 @@
+/*
+Cities on a map are connected by a number of roads. The number of roads between each city is in an array and city  is the starting location. The number of roads from city  to city  is the first value in the array, from city  to city  is the second, and so on.
+
+How many paths are there from city  to the last city in the list, modulo ?
+
+Example
+
+
+There are  roads to city ,  roads to city  and  roads to city . The total number of roads is .
+
+Note
+Pass all the towns Ti for i=1 to n-1 in numerical order to reach Tn.
+
+Function Description
+
+Complete the connectingTowns function in the editor below.
+
+connectingTowns has the following parameters:
+
+int n: the number of towns
+int routes[n-1]: the number of routes between towns
+Returns
+
+int: the total number of routes, modulo 1234567.
+Input Format
+The first line contains an integer T, T test-cases follow.
+
+Each test-case has 2 lines.
+The first line contains an integer N (the number of towns).
+The second line contains N - 1 space separated integers where the ith integer denotes the number of routes, Ni, from the town Ti to Ti+1
+
+Constraints
+1 <= T<=1000
+2< N <=100
+1 <= routes[i] <=1000
+
+Sample Input
+
+2
+3
+1 3
+4
+2 2 2
+Sample Output
+
+3
+8
+Explanation
+Case 1: 1 route from T1 to T2, 3 routes from T2 to T3, hence only 3 routes.
+Case 2: There are 2 routes from each city to the next, hence 2 * 2 * 2 = 8.
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'connectingTowns' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY routes
+ */
+
+int connectingTowns(int n, vector<int> routes) {
+    int roads = 1;
+    
+    for (int a : routes)
+        roads = (roads * a) % 1234567;
+    
+    return roads;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string routes_temp_temp;
+        getline(cin, routes_temp_temp);
+
+        vector<string> routes_temp = split(rtrim(routes_temp_temp));
+
+        vector<int> routes(n - 1);
+
+        for (int i = 0; i < n - 1; i++) {
+            int routes_item = stoi(routes_temp[i]);
+
+            routes[i] = routes_item;
+        }
+
+        int result = connectingTowns(n, routes);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
